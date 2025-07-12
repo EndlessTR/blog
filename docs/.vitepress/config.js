7651ebf75645fdb1774f824ce01aa3eb76a8c81c
EndlessTR <wangyiding0726@126.com>
2025-07-12 20:53:14 +0800

    添加讲座笔记

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,24 @@
 export default {
     base: '/blog/',
     title: '丁仙的博客',
-    description: 'VitePress我们喜欢你'
+    description: 'VitePress我们喜欢你',
+      themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '笔记', link: '/notes/' },
+    ],
+    sidebar: {
+      '/notes/': [
+        {
+          text: '笔记总览',
+          items: [
+            { text: '25-7-8', link: '/notes/25-7-8.md' }
+            
+          ]
+        }
+      ]
+    }
+  }
+
   }
   
